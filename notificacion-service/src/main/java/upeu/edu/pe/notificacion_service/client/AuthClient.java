ac517186b19149bcd5193b202f2eab9bdd158993
levidv22 <levidiaz295@gmail.com>
2025-07-22 07:19:31 -0500

    Update AuthClient.java

--- notificacion-service/src/main/java/upeu/edu/pe/notificacion_service/client/AuthClient.java
+++ notificacion-service/src/main/java/upeu/edu/pe/notificacion_service/client/AuthClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import upeu.edu.pe.notificacion_service.beans.LoadBalancerConfiguration;
 
-@FeignClient(name = "auth-service", url = "https://ms-auth-service-es6r.onrender.com")
+@FeignClient(name = "auth-service")
 public interface AuthClient {
     @GetMapping(path = "auth-service/auth/admin-exists")
     boolean adminExists();
